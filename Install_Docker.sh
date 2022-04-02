f4a36c1746c24cc0de79ccdffcf8ae2795c0ad61
David <jamcarbon@gmail.com>
2022-04-02 23:36:10 +1300

    update

--- Install_Docker.sh
+++ Install_Docker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt update ;
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common ;
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ;
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" ;
 
-apt-cache policy docker-ce
+apt-cache policy docker-ce ;
 
-sudo apt install docker-ce
+sudo apt install docker-ce ;
 
-sudo systemctl status docker
+sudo systemctl status docker 
--- Install_Polkadot_Node.sh
+++ Install_Polkadot_Node.sh
@@ -2,5 +2,6 @@
 
 # you can change the name of the node, for this example is "jamcarbon_PokadotFullNode"
 
-sudo docker run -p 9944:9944 -p 9615:9615 parity/polkadot:v0.9.18 --name "jamcarbon_PokadotFullNode" --rpc-external --ws-external --prometheus-external
+sudo docker run -p 9944:9944 -p 9615:9615 parity/polkadot:v0.9.18 --name "jamcarbon_PokadotFullNode" --rpc-external --ws-external --prometheus-external ;
 
+echo "Polkadot Node has been sucesfully deployed"
