f4b7cff0639c7960fa76741524df64384457eca0
David <jamcarbon@gmail.com>
2022-04-02 23:30:09 +1300

    update

--- Install_Polkadot_Node.sh
+++ Install_Polkadot_Node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#you can change the name of the node, for this example is "jamcarbon_PokadotFullNode"
+# you can change the name of the node, for this example is "jamcarbon_PokadotFullNode"
 
 sudo docker run -p 9944:9944 -p 9615:9615 parity/polkadot:v0.9.18 --name "jamcarbon_PokadotFullNode" --rpc-external --ws-external --prometheus-external
 
