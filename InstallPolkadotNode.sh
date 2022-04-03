a7c55475f0348a52dab9d79d8bc1cf34efc90103
David <jamcarbon@gmail.com>
2022-04-03 17:40:20 +1200

    update

--- InstallPolkadotNode.sh
+++ InstallPolkadotNode.sh
@@ -5,4 +5,3 @@
 
 sudo docker run -p 9944:9944 -p 9615:9615 parity/polkadot:v0.9.18 --name "jamcarbon_PokadotFullNode" --rpc-external --ws-external --prometheus-external ;
 
-echo "Polkadot Node has been sucesfully deployed"
